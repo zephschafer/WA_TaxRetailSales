4cd2df41260a960a89c3e8a394474d9ba61bb4d5
Zeph Schafer <zephschafer@Zephs-MacBook-Air.local>
2018-11-27 16:19:01 -0800

    Reset initial source load

--- static/js/stackedchart.js
+++ static/js/stackedchart.js
@@ -5,7 +5,7 @@ d3.csv("static/data/STR_WA_C_N1_A_W.csv", function(data) {
 
   var data = data;
   // Set Initial Geography
-  var countyselection = "Adams"
+  var countyselection = "Statewide"
   drawViz(data, countyselection)
 
 });
